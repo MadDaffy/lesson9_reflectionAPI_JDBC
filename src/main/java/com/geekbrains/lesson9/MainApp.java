66f2274b83755f8a99bab6d75b7bad6fb3e0172a
MadDaffy <kuzutaka@yandex.ru>
2022-12-19 20:20:24 +0300

    homework

--- src/main/java/com/geekbrains/lesson9/MainApp.java
+++ src/main/java/com/geekbrains/lesson9/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp {
 
 
     public static void main(String[] args) throws ClassNotFoundException {
-//        TableHandler.buildTable(Student.class);
-        TableHandler.addObjectInTable(new Student(1, "HYI"));
+        TableHandler.buildTable(Student.class);
+        TableHandler.addObjectInTable(new Student(1, "Vano"));
     }
 }
